5bb9b02db89b7bb99c093f9413690d8b1a0fa05c
Khuldoonqataesheh <kqataesheh@gmail.com>
2021-04-28 14:43:46 +0300

    signin page

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
-body = $('body')
+body = $('.signInPage')
+
+body_1 = $('.signUpPage')
+
 div = $('<div class="header" ></div>')
 header = $('<div> <h1  class="h1">Meraki Platform</h1></div> ')
 header_2=$('<div><h2 class="h2">Meraki Platform</h2></div>')
@@ -8,8 +11,8 @@ userName = $(' <div><input type="text" placeholder="Username"></div>')
 password = $(' <div><input type="password" placeholder="Password"></div>')
 signUp = $(' <div><button class="Button"> Sign Up</button></div>')
 agreement = $('<div> <p id="agreement" >By signing up, you agree to our Terms ,<br> Data Policy and Cookies Policy .</p></div>')
-signIn = $(' <div class ="signIn"><p id="agreement" >Have an account? <a href="#signIn"> SignIn</a></p> </div>')
-body.append(div)
+signIn = $(' <div class ="signIn"><p id="agreement" >Have an account? <a href="file:///D:/MERAKI/Projects/MERAKI_Academy_Project_2/MERAKI_Academy_Project_2/index_2.html"> SignIn</a></p> </div>')
+body_1.append(div)
 div.append(header)
 div.append(header_2)
 div.append(email)
@@ -18,10 +21,32 @@ div.append(userName)
 div.append(password)
 div.append(signUp)
 div.append(agreement)
-body.append(signIn)
-
-
-
+body_1.append(signIn)
+const signInClick = $("a");
+signUpPage = $('.signUpPage')
+let a = false
+signInClick.on("click", () => {
+  
+       return display_2()
+  });
+  
+  
+      
+    
+        div_2 = $('<div class="header" ></div>')
+        header_2 = $('<div> <h1  class="h1">Meraki Platform</h1></div> ')
+        email_2 = $(' <div><input type="text" placeholder="Email"></div>')
+        password_2 = $(' <div><input type="password" placeholder="Password"></div>')
+        signIn_2 = $(' <div><button class="Button"> SignIn</button></div>')
+        signIn = $(' <div class ="signIn"><p id="agreement" >Do not have an account? <a href="file:///D:/MERAKI/Projects/MERAKI_Academy_Project_2/MERAKI_Academy_Project_2/index.html#contact"> SignIn</a></p> </div>')
+        body.append(div_2)
+        div_2.append(header_2)
+        div_2.append(email_2)
+        div_2.append(password_2)
+        div_2.append(signIn_2)
+        body.append(signIn)
+        
+  
 
 
 
