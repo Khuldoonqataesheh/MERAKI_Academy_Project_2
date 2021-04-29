03023c88c72e35bcca5c5c3c6456d4d429d5ae39
Khuldoonqataesheh <kqataesheh@gmail.com>
2021-04-29 22:11:58 +0300

    remove two pages

--- main.js
+++ main.js
@@ -1,17 +1,23 @@
-body = $(".signInPage");
+body = $(".sigInPage");
 body_1 = $(".signUpPage");
 div = $('<div class="header" ></div>');
 header = $('<div> <h1  class="h1">Meraki Platform</h1></div> ');
 header_2 = $('<div><h2 class="h2">Meraki Platform</h2></div>');
-email = $(' <div><input type="text" placeholder="Email" class = "email"></div>');
-displayName = $(' <div><input class = "displayName" type="text" placeholder="display Name"></div>');
-password = $(' <div><input type="password" placeholder="Password" class = "password"></div>');
+email = $(
+  ' <div><input type="text" placeholder="Email" class = "email"></div>'
+);
+displayName = $(
+  ' <div><input class = "displayName" type="text" placeholder="Display Name"></div>'
+);
+password = $(
+  ' <div><input type="password" placeholder="Password" class = "password"></div>'
+);
 signUp = $(' <div><button class="signUp" > Sign Up</button></div>');
 agreement = $(
   '<div> <p id="agreement" >By signing up, you agree to our Terms ,<br> Data Policy and Cookies Policy .</p></div>'
 );
 signIn = $(
-  ' <div class ="signIn"><p id="agreement" >Have an account? <a href="file:///D:/MERAKI/Projects/MERAKI_Academy_Project_2/MERAKI_Academy_Project_2/index_2.html"> SignIn</a></p> </div>'
+  ' <div class ="signIn"><p id="agreement" >Have an account? </p><button class="sign" > Sign Up</button> </div>'
 );
 body_1.append(div);
 div.append(header);
@@ -22,27 +28,34 @@ div.append(password);
 div.append(signUp);
 div.append(agreement);
 body_1.append(signIn);
-const signInClick = $("a");
-signUpPage = $(".signUpPage");
-let a = false;
-signInClick.on("click", () => {
-  return display_2();
-});
 
 div_2 = $('<div class="header" ></div>');
 header_2 = $('<div> <h1  class="h1">Meraki Platform</h1></div> ');
-email_2 = $(' <div><input type="text" placeholder="Email"></div>');
-password_2 = $(' <div><input type="password" placeholder="Password"></div>');
+displayName_2 = $(' <div><input class = "displayName_2" type="text" placeholder="display Name"></div>');
+password_2 = $(' <div><input type="password" placeholder="Password" class = "password_2"></div>');
 signIn_2 = $(' <div><button class="signIn_2"> SignIn</button></div>');
 signIn = $(
   ' <div class ="signIn"><p id="agreement" >Do not have an account? <a href="file:///D:/MERAKI/Projects/MERAKI_Academy_Project_2/MERAKI_Academy_Project_2/index.html#contact"> SignUp</a></p> </div>'
 );
 body.append(div_2);
 div_2.append(header_2);
-div_2.append(email_2);
+div_2.append(displayName_2);
 div_2.append(password_2);
 div_2.append(signIn_2);
 body.append(signIn);
+signUpPage = $(".signUpPage");
+$('.sign').on('click',()=>{
+    signUpPage.css({
+        "display": "none",
+       
+      });
+      $(".sigInPage").css({
+        "display": "block",
+       
+      });
+})
+
+
 displayNameForUsers = [
   "A01_Abdalafo",
   "A03_Ahmad",
@@ -95,48 +108,85 @@ displayNameForUsers = [
   "C28_Hussain",
   "C31_Tawfeeq",
 ];
-arr = []
-obj = {}
-let text = $('.displayName')
-let mail = $('.email')
-let pass = $('.password')
-const signUpClick = $(".signUp");
-signUpClick.on("click",  (username, password) => {
-  for(let i = 0 ;i<displayNameForUsers.length;i++) {
-       if (displayNameForUsers[i].toLowerCase()===text.val().toLowerCase()) {
-     obj.displayName = displayNameForUsers[i]
-     text.val("")
-       }
-    }
-    if(obj.displayName ===undefined){
-        text.val("wrong display name")
-    }
-  
-    
-  
-    if (mail.val().length <= 6|| !mail.val().includes('@') || !mail.val().includes('.com')){
-        mail.val("email must be at least 6 characters long");
-    }
-    if (pass.val().length < 8) {
-        pass.val("password must be at least 8 characters long");
-    }
-    if (mail.val().length > 6 && pass.val().length >= 8 && obj.displayName) {
-        if (mail.val().includes('@') && mail.val().includes('.com')) {
-         {
-            obj.email = mail.val()
-            obj.pass = pass.val()
-            arr.push(obj)
-            mail.val("")
-            pass.val("")
-            console.log(arr)
-            location.href = "index_3"
-            
-          }
-        }
-    }
-  
+arr = [];
 
+let text = $(".displayName");
+let mail = $(".email");
+let pass = $(".password");
+const signUpClick = $(".signUp");
+signUpClick.on("click", () => {
+    obj = {};
+  for (let i = 0; i < displayNameForUsers.length; i++) {
+    if (displayNameForUsers[i].toLowerCase() === text.val().toLowerCase()) {
+      obj.displayName = displayNameForUsers[i];
+      text.val("");
+    }
+  }
+  if (obj.displayName === undefined) {
+    text.val("wrong display name");
+  }
+
+  if (
+    mail.val().length <= 6 ||
+    !mail.val().includes("@") ||
+    !mail.val().includes(".com")
+  ) {
+    mail.val("email must be at least 6 characters long");
+  }
+  if (pass.val().length < 8) {
+    pass.val("password must be at least 8 characters long");
+  }
+  if (mail.val().length > 6 && pass.val().length >= 8 && obj.displayName) {
+    if (mail.val().includes("@") && mail.val().includes(".com")) {
+      {
+        obj.email = mail.val();
+        obj.pass = pass.val();
+        arr.push(obj);
+        mail.val("");
+        pass.val("");
+        console.log(arr);
+      
+      }
+    }
+  }
 });
+
+let text_2 = $(".displayName_2");
+let pass_2 = $(".password_2");
+const signInClick = $(".signIn_2");
+signInClick.on("click", () => {
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i].displayName.toLowerCase() === text_2.val().toLowerCase()&&
+      (arr[i].pass === pass_2.val())) {
+        text_2.val("");
+        pass_2.val("");
+        console.log(arr[i])
+      }
+    }
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 /*const isValidUser =  (loginInfo) => {
     const email = loginInfo.email;
